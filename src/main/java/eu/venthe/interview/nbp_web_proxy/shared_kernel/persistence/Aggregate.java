122c5f8a4d2009e1cdd742a931d18f1b3a1053cf
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2024-09-14 14:14:07 +0200

    Add basic Domain Repository interface

---
+++ src/main/java/eu/venthe/interview/nbp_web_proxy/shared_kernel/persistence/Aggregate.java
@@ -0,0 +1,5 @@
+package eu.venthe.interview.nbp_web_proxy.shared_kernel.persistence;
+
+public interface Aggregate<ID> {
+    ID getId();
+}
---
+++ src/main/java/eu/venthe/interview/nbp_web_proxy/shared_kernel/persistence/DomainRepository.java
@@ -0,0 +1,5 @@
+package eu.venthe.interview.nbp_web_proxy.shared_kernel.persistence;
+
+public interface DomainRepository<ID, AGGREGATE extends Aggregate<ID>> {
+    ID save(AGGREGATE aggregate);
+}
